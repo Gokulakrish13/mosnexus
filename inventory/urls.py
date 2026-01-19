c849b63f30577b2a9ee27447ecdf7c3cbc556ec4
Gokul <107119043+Gokulakrish13@users.noreply.github.com>
2026-01-19 09:37:31 +0530

    Update URL configuration to include static files
    Added static URL configuration for serving static files in debug mode.

--- inventory/urls.py
+++ inventory/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from products import views
+from django.conf import settings # new
+from  django.conf.urls.static import static #new   
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,4 +29,6 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)        
+
