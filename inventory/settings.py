3e59444a3cf2f552f34886589f3158dc035109a4
Gokul <107119043+Gokulakrish13@users.noreply.github.com>
2026-03-19 21:23:13 +0530

    Update Django settings for inventory project

--- inventory/settings.py
+++ inventory/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g#=(mb2401nl!s-vr3nzjdsjti&6v7#wiu2+m%xfpyjki@bx73
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','130.141.135.225','14.194.95.251','13.49.238.18','0.0.0.0','igtmosnexus.com']
+ALLOWED_HOSTS = ['127.0.0.1','13.62.76.13']
 
 
 # Application definition
